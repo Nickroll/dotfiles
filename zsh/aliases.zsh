0c74b3c77e584b1cb7a4701b121892264f0e9010
Nickroll <rick.nickroller@gmail.com>
2020-07-01 19:47:29 -0400

    Adding zsh to dotfiles

---
+++ zsh/aliases.zsh
@@ -0,0 +1,12 @@
+# Aliases
+
+alias ls='ls -Fh --color=auto'
+alias rm='rm -i'
+alias grep='grep --color=auto'
+alias cp='cp -iv'
+alias mv='mv -iv'
+alias mkdir='mkdir -pv'
+alias ll='ls -altr'
+alias szp='source ~/.config/zshrc'
+alias ezp='nvim ~/.config/zshrc'
+alias eni='nvim ~/.config/nvim/init.vim'
