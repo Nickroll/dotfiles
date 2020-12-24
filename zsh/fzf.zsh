4e7cca06f371854c7c6c7ae397f33081122ac332
Nickroll <rick.nickroller@gmail.com>
2020-12-23 19:55:15 -0500

    Adding new fzf options and colors

---
+++ zsh/fzf.zsh
@@ -0,0 +1,1 @@
+export FZF_DEFAULT_OPTS="--no-mouse --height 50% -1 --reverse --multi --inline-info --preview='(bat --style=numbers --color=always --line-range :500 {})' --color=bg:#222433,fg+:#4EB5EB,hl:#E567EC,bg+:#4d5275,hl+:#abad0c"
