52546d631ed3df03c1524c2dee2fecc697078963
Nickroll <rick.nickroller@gmail.com>
2021-01-12 01:27:40 -0500

    updating polybar and adding fd for fzf

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,1 +1,2 @@
 export FZF_DEFAULT_OPTS="--no-mouse --height 50% -1 --reverse --multi --inline-info --preview='(bat --style=numbers --color=always --line-range :500 {})' --color=bg:#222433,fg+:#4EB5EB,hl:#E567EC,bg+:#4d5275,hl+:#abad0c"
+export FZF_DEFAULT_COMMAND="fdfind --type f --hidden --follow --exclude .git"
